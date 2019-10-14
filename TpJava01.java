ea639a8c4700e6d0b2ce62260733ed9bee398b06
RAZAFIMANJATO <samoela.razafi@gmail.com>
2019-10-14 10:14:38 +0200

    My first files on git

---
+++ TpJava01.java
@@ -0,0 +1,14 @@
+
+public class TpJava01 {
+
+	/**
+	 * 
+	 * @param args Les arguments du programme
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for ( int i = 0; i<args.length ; ++i) {
+			System.out.println("Bonjour "+ args[i]);
+		}
+	}
+}
